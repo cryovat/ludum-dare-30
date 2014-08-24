8a37fc06ca4b23ca7d42362abf2c6738c28c7934
Jarl Erik Schmidt <github@jarlerik.com>
2014-08-25 00:34:06 +0200

    Mechanics sort of working

--- src/game.ts
+++ src/game.ts
@@ -45,6 +45,29 @@ class GameState {
         this.shipModel = "shuttle";
     }
 
+    hasFuel(): boolean {
+
+        return $.grep(this.cards, (v) => v.type === "fuel").length > 0;
+
+    }
+
+    removeCardFromHand(type: string): boolean {
+
+        var types = $.map(this.cards, (v, i) => v.type),
+            ix = $.inArray(type, types);
+
+        if (ix > -1) {
+            this.cards.splice(ix, 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    removeFuelFromHand(): boolean {
+        return this.removeCardFromHand("fuel");
+    }
+
 }
 
 class GameMain2 {
@@ -54,13 +77,14 @@ class GameMain2 {
     private $shipmake: JQuery;
     private $tooltip: JQuery;
     private $planetDash: JQuery;
+    private svg: D3.Selection;
     private currentPlanet: any;
 
-    private state: GameState = new GameState();
-
+    private state: GameState;
 
     constructor(private container: HTMLElement, private graphWidth: number, private graphHeight: number) {
 
+        this.state = new GameState();
         this.$container = $(container);
     }
 
@@ -78,31 +102,108 @@ class GameMain2 {
 
     }
 
-    playCard(planet: any, type: string, price: number): void {
+    buyCard(planet: any, type: string, price: number): void {
 
-        var after = $.grep(this.state.cards, (e, i) => e.type !== type);
+        this.$tooltip.hide();
 
-        if (this.state.cards.length !== after.length + 1) {
-            alert("You can't play a card you don't have!");
+        var types = $.map(this.state.market, (v, i) => v.type),
+            ix = $.inArray(type, types);
+
+        if (ix < 0) {
+            alert("You can't buy a card that doesn't exist!");
         }
 
-        this.state.cards = after;
-        this.state.credits += price;
+        this.state.market.splice(ix, 1);
+
+        this.state.cards.push(cards[type]);
+        this.state.credits -= price;
 
         this.setPlanet(planet);
 
     }
 
-    dealMarketCards(prices): void {
+    playCard(planet: any, type: string, price: number): void {
 
+        this.$tooltip.hide();
+
+        var types = $.map(this.state.cards, (v, i) => v.type),
+            ix = $.inArray(type, types);
+
+        if (ix < 0) {
+            alert("You can't play a card you don't have!");
+        }
+
+        this.state.cards.splice(ix, 1);
+        this.state.credits += price;
+
+        this.setPlanet(planet);
+    }
+
+    dealMarketCards(prices: { [id: string]: number }, slots: number, clear: boolean): void {
+
+        var marketCards: Card[] = clear ? [] : $.grep(this.state.market, (v, i) => v.type !== "fuel"),
+            types: string[] = <string[]>$.map(prices, (v, i) => i),
+            keys: string[] = $.grep(types, (v, i) => v !== "fuel" && prices[v] <= cards[v].price),
+            key: string,
+            rind: number;
+
+        if (types.length == 0) {
+            this.state.market = [];
+            return;
+        }
+
+        if (keys.length > 0) {
+
+            while (marketCards.length < slots) {
+
+                rind = Math.floor(Math.random() * keys.length);
+
+                marketCards.push(cards[keys[rind]]);
+            }
+
+        }
+
+        if ($.inArray("fuel", types) > -1) {
+            marketCards.push(cards["fuel"]);
+        }
+
+        this.state.market = marketCards;
+    }
+
+    tryMovePlanet(planet: any, graph: D3.Layout.ForceLayout) {
+
+        var links = graph.links(), current = this.currentPlanet;
+
+        if (current === planet) {
+            alert("You are already there!");
+            return;
+        }
+
+        var paths = $.grep(links, (v) => (v.source === current && v.target === planet) || (v.source === planet && v.target === current));
+
+        if (paths.length === 0) {
+            alert("You can only jump one system at a time");
+        } else if (this.state.removeFuelFromHand()) {
+            this.setPlanet(planet);
+        } else {
+            alert("You don't have enough fuel to carry out the jump!");
+        }
+
+        console.log(this.currentPlanet, planet, graph);
     }
 
     setPlanet(planet: any): void {
 
+        var d = this.$planetDash,
+            $market,
+            $hand,
+            traded: string[] = [], prices: { [id: string]: number; } = {},
+            newPlanet: boolean = this.currentPlanet !== planet,
+            cardCount: number = 0,
+            state = this.state,
+            sellable: number = 0;
+
         this.currentPlanet = planet;
-
-        var d = this.$planetDash, $market, $hand, traded: string[] = [], prices: { [id: string]: number; } = {};
-
 
         this.$credits.text(this.state.credits);
         this.$shipmake.text(ships[this.state.shipModel].name);
@@ -122,33 +223,82 @@ class GameMain2 {
             prices[v[0]] = Math.floor(cards[v[0]].price * v[1]);
         });
 
-        this.dealMarketCards(prices);
+        this.dealMarketCards(prices, planet.slots, newPlanet);
 
-        if (planet.trade.length == 0) {
-            $market.text("There is nothing for sale...");
+        $.each(state.cards, (i, v) => {
+
+            var enable = $.inArray(v.type, traded) != -1 && v.type !== "fuel",
+                card = this.renderCard(v.type, prices[v.type], enable);
+
+            if (!enable) {
+                card.addClass("disabled");
+            } else {
+
+                sellable += prices[v.type];
+
+                card.on("click", () => this.playCard(planet, v.type, prices[v.type]));
+                card.on("mousemove", () => this.showSellTooltip(v.name, prices[v.type]));
+                card.on("mouseout", () => this.$tooltip.hide());
+            }
+
+            $hand.append(card);
+
+            cardCount++;
+
+        });
+
+        $.each(state.market, (i, v) => {
+
+            var playerHandFull = state.cards.length >= ships[state.shipModel].capacity,
+                enable = prices[v.type] <= state.credits && !playerHandFull,
+                card = this.renderCard(v.type, prices[v.type], enable);
+
+            if (!enable) {
+                card.addClass("disabled");
+            } else {
+                card.on("click", () => this.buyCard(planet, v.type, prices[v.type]));
+                card.on("mousemove", () => this.showBuyTooltip(v.name, prices[v.type]));
+                card.on("mouseout", () => this.$tooltip.hide());
+            }
+
+            $market.append(card);
+
+            cardCount++;
+
+        });
+
+        while (cardCount < ships[this.state.shipModel].capacity) {
+            $hand.append($("<span>").html("&nbsp").addClass("cardPlaceholder"));
+            cardCount++;
         }
 
-        if (this.state.cards.length == 0) {
-            $hand.text("Your currently don't have any cards");
-        } else {
-            $.each(this.state.cards, (i, v) => {
 
-                var enable = $.inArray(v.type, traded) != -1 && v.type !== "fuel",
-                    card = this.renderCard(v.type, prices[v.type], enable);                
 
-                if (!enable) {
-                    card.addClass("disabled");
-                } else {
-                    card.on("click", () => this.playCard(planet, v.type, prices[v.type]));
-                    card.on("mousemove", () => this.showSellTooltip(v.name, prices[v.type]));
-                    card.on("mouseout", () => this.$tooltip.hide());
-                }
-
-                $hand.append(card);
-            });
+        if (!this.state.hasFuel() && (!prices["fuel"] || prices["fuel"] > (this.state.credits + sellable))) {
+            alert("You've run out of fuel. You're at the journey's end...");
+            d.empty();
+            d.append($("<div>").addClass("gameOver").append($("<h2>").text("Game over")));
         }
 
         d.show();
+    }
+
+    showGameOver() {
+
+    }
+
+    showTooltip() {
+
+        var graphicsElem = <HTMLElement>this.svg[0][0],
+            bodyRect = document.body.getBoundingClientRect(),
+            elemRect = graphicsElem.getBoundingClientRect(),
+            offsetLeft = elemRect.left - bodyRect.left + elemRect.width,
+            offsetTop = elemRect.top - bodyRect.top;
+
+        this.$tooltip.css("left", offsetLeft);
+        this.$tooltip.css("top", offsetTop);
+        this.$tooltip.show();
+
     }
 
     setRandomPlanet(planet: any[]) {
@@ -157,6 +307,24 @@ class GameMain2 {
 
         this.setPlanet(planet[ix]);
 
+    }
+
+    showBuyTooltip(name: string, price: number) {
+
+        var t = this.$tooltip, $summary;
+
+        t.empty();
+        t.append($("<h2>").text("Action"));
+
+        t.append($summary = $("<div>").addClass("summary"));
+
+        t.append("Click card to buy ");
+        t.append($("<strong>").text(name));
+        t.append(" for ");
+        t.append(price.toString());
+        t.append("credits.");
+
+        this.showTooltip();
     }
 
     showSellTooltip(name: string, price: number) {
@@ -174,9 +342,7 @@ class GameMain2 {
         t.append(price.toString());
         t.append("credits.");
 
-        t.show();
-
-
+        this.showTooltip();
     }
 
     showShipTooltip() {
@@ -188,7 +354,7 @@ class GameMain2 {
 
         t.append($("<div>").html(ship.description).addClass("summary"));
 
-        t.show();
+        this.showTooltip();
 
     }
 
@@ -221,13 +387,13 @@ class GameMain2 {
             $ul.append($("<li>").text("None"));
         }
 
-        t.show();
+        this.showTooltip();
     }
 
     start(): void {
 
 
-        var color = d3.scale.category20();
+        var color = d3.scale.category20(), iterations: number = 150;
 
         var $dl = $("<dl>");
         $dl.append("<dt>Ship</dt>");
@@ -239,12 +405,12 @@ class GameMain2 {
         this.$shipmake.on("mouseover", () => this.showShipTooltip());
         this.$shipmake.on("mouseout", () => this.$tooltip.hide());
 
-        var svg = d3.select(this.container).append("svg")
+        this.svg = d3.select(this.container).append("svg")
             .attr("width", this.graphWidth)
             .attr("height", this.graphHeight);
 
         this.$tooltip = $("<div>").addClass("planetTooltip");
-        this.$tooltip.appendTo(this.$container);
+        this.$tooltip.appendTo(document.body);
         this.$tooltip.hide();
 
         this.$planetDash = $("<div>").addClass("planetDashboard");
@@ -252,7 +418,6 @@ class GameMain2 {
         this.$planetDash.hide();
 
         var force = d3.layout.force()
-            .charge(-120)
             .linkDistance(function (d) { return d.value * 10; })
             .size([this.graphWidth, this.graphHeight]);
 
@@ -263,16 +428,17 @@ class GameMain2 {
             }
 
             force
+                .charge(-150)
                 .nodes(graph.nodes)
                 .links(graph.links)
                 .start();
 
-            var link = svg.selectAll("link")
+            var link = this.svg.selectAll("link")
                 .data(graph.links)
                 .enter().append("line")
                 .attr("class", "link");
 
-            var node = svg.selectAll("node")
+            var node = this.svg.selectAll("node")
                 .data(graph.nodes)
                 .enter().append("circle")
                 .attr("class", "node")
@@ -280,7 +446,7 @@ class GameMain2 {
                 .style("fill", function (d) { return color(d.group); })
                 .on("mouseover", (d) => this.showPlanetTooltip(d))
                 .on("mouseout", (d) => this.$tooltip.hide())
-                .on("click", (d) => this.setPlanet(d))
+                .on("click", (d) => this.tryMovePlanet(d, force))
                 .call(force.drag);
 
             node.append("title")
@@ -295,6 +461,12 @@ class GameMain2 {
                 node.attr("cx", function (d) { return d.x; })
                     .attr("cy", function (d) { return d.y; });
             });
+
+
+            for (var i = iterations * iterations; i > 0; --i) force.tick();
+            force.stop();
+
+            node.each(d => d.fixed = true);
 
             this.setRandomPlanet(force.nodes());
 
