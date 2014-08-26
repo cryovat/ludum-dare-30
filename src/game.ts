04de3d9ce6c3b9b9e3af60f21eb6a370fe9c42f0
Jarl Erik Schmidt <github@jarlerik.com>
2014-08-26 02:41:35 +0200

    Getting there

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,6 @@
-﻿class Ship {
+﻿/// <reference path="starfield.ts" />
+
+class Ship {
 
     constructor(public name: string, public capacity: number, public description: string) {
 
@@ -7,7 +9,7 @@
 }
 
 var ships: { [id: string]: Ship } = {
-    "shuttle": new Ship("MK I Shuttlecraft", 5, "The shuttles were never known for their elegance or defensive ability, but they're reliable and often the only thing an independent trader can afford. <strong>The shuttle can hold five items.</strong>")
+    "shuttle": new Ship("MK I Shuttlecraft", 6, "The shuttles were never known for their elegance or defensive ability, but they're reliable and often the only thing an independent trader can afford. <strong>The shuttle can hold five items.</strong>")
 };
 
 class Card {
@@ -23,10 +25,12 @@ var addCard = (card: Card) => {
     cards[card.type] = card;
 };
 
-addCard(new Card("fuel", "Fuel", 50));
+addCard(new Card("fuel", "Fuel", 35));
 addCard(new Card("food", "Food", 100));
+addCard(new Card("ore", "Ore", 100));
 addCard(new Card("medicine", "Medicine", 200));
 addCard(new Card("luxury", "Luxury goods", 500));
+addCard(new Card("xxx", "Collectible", 100));
 addCard(new Card("tech", "Technology", 500));
 
 class GameState {
@@ -35,13 +39,15 @@ class GameState {
     public credits: number;
     public shipModel: string;
 
+    public alive: boolean = true;
+
     constructor() {
 
         this.cards.push(cards["fuel"]);
         this.cards.push(cards["fuel"]);
         this.cards.push(cards["food"]);
 
-        this.credits = 100;
+        this.credits = 200;
         this.shipModel = "shuttle";
     }
 
@@ -78,6 +84,8 @@ class GameMain2 {
     private $tooltip: JQuery;
     private $planetDash: JQuery;
     private svg: D3.Selection;
+    private lineH: D3.Selection;
+    private lineV: D3.Selection;
     private currentPlanet: any;
 
     private state: GameState;
@@ -92,6 +100,7 @@ class GameMain2 {
 
         var $card = $("<a>").addClass("card").addClass(type);
 
+
         if (price && showPrice) {
             $card.append($("<span>").addClass("sellPrice").text(price));
         } else {
@@ -99,7 +108,6 @@ class GameMain2 {
         }
 
         return $card;
-
     }
 
     buyCard(planet: any, type: string, price: number): void {
@@ -172,6 +180,17 @@ class GameMain2 {
 
     tryMovePlanet(planet: any, graph: D3.Layout.ForceLayout) {
 
+        if (!this.state.alive) {
+
+            if (this.state.credits > 10000) {
+                alert("You don't need to travel anymore. These days you hire other people to travel for you while you count your money.");
+            } else {
+                alert("You failed to make it as an interstellar trader.");
+            }
+
+            return;
+        }
+
         var links = graph.links(), current = this.currentPlanet;
 
         if (current === planet) {
@@ -190,6 +209,25 @@ class GameMain2 {
         }
     }
 
+    setCrosshair(planet: any) {
+
+        var x = Math.floor(planet.x),
+            y = Math.floor(planet.y);
+
+        this.lineH
+            .attr("x1", 0)
+            .attr("y1", y)
+            .attr("x2", this.graphWidth)
+            .attr("y2", y);
+
+        this.lineV
+            .attr("x1", x)
+            .attr("y1", 0)
+            .attr("x2", x)
+            .attr("y2", this.graphHeight);
+
+    }
+
     setPlanet(planet: any): void {
 
         var d = this.$planetDash,
@@ -200,6 +238,8 @@ class GameMain2 {
             cardCount: number = 0,
             state = this.state,
             sellable: number = 0;
+
+        this.setCrosshair(planet);
 
         this.currentPlanet = planet;
 
@@ -277,8 +317,6 @@ class GameMain2 {
 
             $market.append(card);
 
-            cardCount++;
-
         });
 
         while (cardCount < ships[this.state.shipModel].capacity) {
@@ -287,17 +325,57 @@ class GameMain2 {
         }
 
 
+        if (this.state.credits > 10000) {
 
-        if (!this.state.hasFuel() && (!prices["fuel"] || prices["fuel"] > (this.state.credits + sellable))) {
             d.empty();
-            d.append($("<div>").addClass("gameOver").append($("<h2>").text("Game over")));
-            alert("You've run out of fuel. You're at the journey's end...");
-        }
 
-        d.show();
+            this.state.alive = false;
+
+            this.showVictory();
+
+        } else if (!this.state.hasFuel() && (!prices["fuel"] || prices["fuel"] > (this.state.credits + sellable))) {
+
+            d.empty();
+
+            this.state.alive = false;
+
+            this.showGameOver();
+        }
+        else {
+            d.show();
+        }
     }
 
     showGameOver() {
+
+
+        this.$planetDash.hide();
+
+        var $gameOver = $("<div>").addClass("gameover");
+        $gameOver.append($("<h2>").text("Game Over"));
+        $gameOver.hide();
+
+        this.$container.append($gameOver);
+
+        alert("You've run out of fuel and money. Disillusioned, you sell your spaceship and try to pay your debts by taking work in a local mine. You toil away your years and never set foot in space again.");
+
+        $gameOver.fadeIn(1000);
+
+    }
+
+    showVictory() {
+
+        this.$planetDash.hide();
+
+        var $gameOver = $("<div>").addClass("victory");
+        $gameOver.append($("<h2>").text("You win"));
+        $gameOver.hide();
+
+        this.$container.append($gameOver);
+
+        alert("Amassing so much money has made you wary of space travel, and you decide to retire to a peaceful life of eating caviar and counting money. Congratulations, you win!");
+
+        $gameOver.fadeIn(1000);
 
     }
 
@@ -446,9 +524,13 @@ class GameMain2 {
 
                 $li = $("<li>").text(cards[g[0]].name);
 
-                if (g[1] < 0.7) {
+                if (g[1] <= 0.7) {
                     $li.addClass("cheap");
-                } else if (g[1] > 1.2) {
+                } else if (g[1] > 6) {
+                    $li.addClass("legendary");
+                } else if (g[1] > 1.3) {
+                    $li.addClass("premium");
+                } else if (g[1] > 1) {
                     $li.addClass("expensive");
                 }
 
@@ -479,6 +561,14 @@ class GameMain2 {
         this.svg = d3.select(this.container).append("svg")
             .attr("width", this.graphWidth)
             .attr("height", this.graphHeight);
+
+        this.lineH = this.svg
+            .append("line")
+            .attr("class", "crosshair");
+
+        this.lineV = this.svg
+            .append("line")
+            .attr("class", "crosshair");
 
         this.$tooltip = $("<div>").addClass("planetTooltip");
         this.$tooltip.appendTo(document.body);
@@ -517,7 +607,7 @@ class GameMain2 {
 
             var node =
                 gnodes.append("circle")
-                    .attr("class", "node")
+                    .attr("class", (d) => "node " + d.name.toLowerCase())
                     .attr("r", function (d) { return d.radius; })
                     .style("fill", function (d) { return color(d.group); })
                     .on("mouseover", (d) => this.showPlanetTooltip(d))
@@ -527,7 +617,7 @@ class GameMain2 {
 
             gnodes.append("text")
                 .attr("class", "planetLabel")
-                .attr("x", (d) => d.radius +5 )
+                .attr("x", (d) => d.radius + 5)
                 .text(function (d) { return d.short; });
 
             node.append("title")
@@ -541,9 +631,8 @@ class GameMain2 {
 
                 gnodes.attr("transform", function (d) {
                     return 'translate(' + [d.x, d.y] + ')';
-                });  
+                });
             });
-
 
             for (var i = iterations * iterations; i > 0; --i) force.tick();
             force.stop();
@@ -558,6 +647,9 @@ class GameMain2 {
 }
 
 window.onload = () => {
+
+
+    var starfield = new Starfield(document);
     var el = document.getElementById('game');
 
     var game = new GameMain2(el, 640, 330);
---
+++ src/starfield.ts
@@ -0,0 +1,97 @@
+﻿// Based on http://threejs.org/examples/webgl_particles_sprites.html
+
+class Starfield {
+
+    private container: HTMLDivElement;
+    private camera: THREE.PerspectiveCamera;
+    private scene: THREE.Scene;
+
+    private renderer : THREE.Renderer;
+
+    constructor(document: HTMLDocument, private clouds : number = 5) {
+
+        this.container = document.createElement("div");
+        this.container.className = "cluster";
+        document.body.appendChild(this.container);
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
+        this.camera.position.z = 1000;
+
+        this.scene = new THREE.Scene();
+        this.scene.fog = new THREE.FogExp2(0x000000, 0.0008);
+
+        var geometry = new THREE.Geometry();
+
+        for (var i = 0; i < 1000; i++) {
+
+            var vertex = new THREE.Vector3();
+            vertex.x = Math.random() * 2000 - 1000;
+            vertex.y = Math.random() * 2000 - 1000;
+            vertex.z = Math.random() * 2000 - 1000;
+
+            geometry.vertices.push(vertex);
+
+        }
+
+        var tex = THREE.ImageUtils.loadTexture("Content/star.png");
+
+        var sprite = tex;
+        var size = 10;
+
+        var material = new THREE.PointCloudMaterial({ size: size, map: tex, blending: THREE.AdditiveBlending, depthTest: false, transparent: true });
+        material.color.setHSL(1, 0.2, 0.5);
+
+        for (i = 0; i < clouds; i++) {
+
+            var cloud = new THREE.PointCloud(geometry, material);
+
+            cloud.rotation.x = Math.random() * 6;
+            cloud.rotation.y = Math.random() * 6;
+            cloud.rotation.z = Math.random() * 6;
+
+            this.scene.add(cloud);
+        }
+
+        this.renderer = new THREE.WebGLRenderer({ clearAlpha: 1 });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.container.appendChild(this.renderer.domElement);
+
+        requestAnimationFrame(() => this.render());
+
+        window.addEventListener("resize", () => {
+
+            var halfX = window.innerWidth / 2;
+            var halfY = window.innerHeight / 2;
+
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+        }, false);
+
+    }
+
+    render() {
+
+        var time = Date.now() * 0.00005;
+
+        for (var i = 0; i < this.scene.children.length; i++) {
+
+            var obj = this.scene.children[i];
+
+            if (obj instanceof THREE.PointCloud) {
+                obj.rotation.y = time * 0.05 * (i < this.clouds / 2 ? i + 1 : - (i + 1));
+            }
+        };
+
+
+        this.renderer.render(this.scene, this.camera);
+
+
+
+        requestAnimationFrame(() => this.render());
+
+    }
+
+}
